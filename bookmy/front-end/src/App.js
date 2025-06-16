399546d269054a5c0dd02d42c46fffbad559f1ca
amareshwar3 <amareshwaranampally@gmail.com>
2025-06-16 22:34:11 +0530

    Reusable Header component with navigation and branding

--- bookmy/front-end/src/App.js
+++ bookmy/front-end/src/App.js
@@ -1,7 +1,20 @@
+import Header from './Components/Header';
 import React from 'react';
 import './App.css';
 import BsState from './Context/BsState';
 import Home from './Pages/Home';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="App">
+        {/* rest of your components */}
+      </div>
+    </>
+  );
+}
 
 function App() {
   return (
